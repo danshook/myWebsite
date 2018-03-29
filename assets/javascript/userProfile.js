05b4fb62b12da99219bad6c041969fcf2ec1b8db
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-03-28 18:06:45 -0700

    added artwork and frames

---
+++ assets/javascript/userProfile.js
@@ -0,0 +1,7 @@
+// ******************************************************************
+//              Get the aspect ratio of your Image
+// ******************************************************************
+
+var aspectRatio = imgWidth / imgHeight
+
+var imgWidth = (1 - (2 * frameToArtPct)) * frameWidth
