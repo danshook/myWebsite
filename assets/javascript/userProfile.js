0ebdb3af0893634da2cb48d2a0c8c14105b52a4e
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-03-28 23:21:48 -0700

    resize parallax

--- assets/javascript/userProfile.js
+++ assets/javascript/userProfile.js
@@ -2,6 +2,9 @@
 //              Get the aspect ratio of your Image
 // ******************************************************************
 
-var aspectRatio = imgWidth / imgHeight
+var frameToArtPct = 15 %
+var frameToMatPct = 2.5 %
+
+var aspectRatio = (imgWidth / imgHeight)
 
 var imgWidth = (1 - (2 * frameToArtPct)) * frameWidth
