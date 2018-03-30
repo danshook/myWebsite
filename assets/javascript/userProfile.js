cee1279e14a975d305ffee11f8499e2c9e18e285
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-03-30 15:26:43 -0700

    welcome hinge

--- assets/javascript/userProfile.js
+++ assets/javascript/userProfile.js
@@ -2,9 +2,20 @@
 //              Get the aspect ratio of your Image
 // ******************************************************************
 
-var frameToArtPct = 15 %
-var frameToMatPct = 2.5 %
+// var frameToArtPct = 15 %
+// var frameToMatPct = 2.5 %
 
-var aspectRatio = (imgWidth / imgHeight)
+// var aspectRatio = (imgWidth / imgHeight)
 
-var imgWidth = (1 - (2 * frameToArtPct)) * frameWidth
+// var imgWidth = (1 - (2 * frameToArtPct)) * frameWidth
+
+setTimeout(function() {
+  $(".welcome")
+    .html("enjoy...")
+    .addClass("welcomeHinge");
+  $(".welcome_info").hide();
+}, 4000);
+
+setTimeout(function() {
+  $(".welcomeHinge").addClass("animated hinge");
+}, 5500);
